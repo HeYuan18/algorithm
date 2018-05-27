865f61513acdd5c1770770b11c73923a7df54829
karma1996 <karma1996@foxmail.com>
2018-05-27 11:46:05 +0800

    快速排序

--- sort/quickS.c
+++ sort/quickS.c
@@ -17,20 +17,31 @@ void swap(int* x, int* y)
 int partition_medianValue(int* const arr, int l, int r)
 {
     int mid = (l + r) / 2;
-    int i = l, j = r;
-    if(arr[l] > arr[mid])
+    if(l == mid)
     {
-        swap(&arr[l], &arr[mid]);
+        if(arr[l] > arr[r])
+        {
+            swap(&arr[l], &arr[r]);
+        }
+
+        return mid;
+    }
+
+    int i = l, j = r;
+    if(arr[l] > arr[r])
+    {
+        swap(&arr[l], &arr[r]);
     }
     if(arr[mid] > arr[r])
     {
         swap(&arr[mid], &arr[r]);
     }
-    if(arr[l] > arr[r])
+    if(arr[l] > arr[mid])
     {
-        swap(&arr[l], &arr[r]);
+        swap(&arr[l], &arr[mid]);
     }
     swap(&arr[mid], &arr[--j]);
+    mid  = j;
 
     while(1)
     {
@@ -45,7 +56,7 @@ int partition_medianValue(int* const arr, int l, int r)
             break;
         }
     }
-    swap(&arr[i], &arr[r - 1]);
+    swap(&arr[i], &arr[mid]);
 
     return i;
 }
@@ -77,6 +88,7 @@ void quick_sort(int* const arr, int l, int r)
     if(l < r)
     {
         int p = partition_medianValue(arr, l, r);
+        //int p = partition(arr, l, r);
         quick_sort(arr, l, p - 1);
         quick_sort(arr, p + 1, r);
     }
@@ -94,11 +106,18 @@ void print_arr(int arr[], int len)
 
 int main()
 {
-    int arr[] = {4,1,3,5,7,2,6};
+    int x[] = {4,1,3,5,2,2,6};
+    int y[] = {1,2,3,4,5,6,7};
+    int z[] = {0,2,4,4,4,5,1};
 
-    quick_sort(arr, 0, 6);
+    quick_sort(x, 0, 6);
+    print_arr(x, 7);
 
-    print_arr(arr, 7);
+    quick_sort(y, 0, 6);
+    print_arr(y, 7);
+
+    quick_sort(z, 0, 6);
+    print_arr(z, 7);
 
     return 0;
 }
